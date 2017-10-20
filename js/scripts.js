336f891abe9a393b013aefcfc637d112dec250ca
Kamil <kamil.iskra@outlook.com>
2017-10-20 12:07:58 +0200

    smooth scroll navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,51 @@
-// Google Maps settings
-function regular_map() {
-    var var_location = new google.maps.LatLng(50.042182, 22.006649);
+(function ($) {
+    "use strict";
+    
+    // Google Maps settings
+    function regular_map() {
+        var var_location = new google.maps.LatLng(50.042182, 22.006649);
 
-    var var_mapoptions = {
-        center: var_location,
-        zoom: 14
-    };
+        var var_mapoptions = {
+            center: var_location,
+            zoom: 14
+        };
 
-    var var_map = new google.maps.Map(document.getElementById("map-container"),
-        var_mapoptions);
+        var var_map = new google.maps.Map(document.getElementById("map-container"),
+            var_mapoptions);
 
-    var var_marker = new google.maps.Marker({
-        position: var_location,
-        map: var_map,
-        title: "Office"
+        var var_marker = new google.maps.Marker({
+            position: var_location,
+            map: var_map,
+            title: "Office"
+        });
+    }
+    // Initialize maps
+    google.maps.event.addDomListener(window, 'load', regular_map);
+
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
     });
-}
 
-// Initialize maps
-google.maps.event.addDomListener(window, 'load', regular_map);
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+})(jQuery); // End of use strict
